9aca54445c67bf78e7ff96741b75b712f8696be4
iain <iain.buchanan@inthepocket.mobi>
2016-12-03 18:37:31 +0100

    Initial Commit

---
+++ FaderBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FaderBar
+//
+//  Created by iain on 03/12/16.
+//  Copyright © 2016 iain. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
